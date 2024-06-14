70715d36b3024ba026f0bab8afa175af3b00b40d
eonduck2 <whdtnxd@gmail.com>
2024-06-14 11:53:34 +0900

    프로미스 반환값 설정

--- 20240614_네트워크 및 API (Protocol & REST API)_평가제출_이종수/src/app.mjs
+++ 20240614_네트워크 및 API (Protocol & REST API)_평가제출_이종수/src/app.mjs
@@ -1,4 +1,11 @@
 import resources from "./modules/AJAX/resources.js";
 import xhr from "./modules/AJAX/ajax.js";
 
-console.log(xhr(resources.albums));
+console.log(
+  xhr(resources.albums).then((res) => {
+    if (res.ok) {
+      console.log(res);
+      return res.json();
+    }
+  })
+);
--- 20240614_네트워크 및 API (Protocol & REST API)_평가제출_이종수/src/modules/AJAX/ajax.js
+++ 20240614_네트워크 및 API (Protocol & REST API)_평가제출_이종수/src/modules/AJAX/ajax.js
@@ -1,22 +1,24 @@
 /**
  * @eonduck2
  * @param {String} resource 요청 api의 자원
- * * @param {Number} rowNum id 값으로 검색, 전달된 값 없을 시, 전체 리스트 조회
+ * @param {Number} rowNum id 값으로 검색, 전달된 값 없을 시, 전체 리스트 조회
  */
 export default (resource, rowNum) => {
-  const url = `https://jsonplaceholder.typicode.com/${resource}/${
-    rowNum ?? ""
-  }`;
+  return new Promise((reslove, reject) => {
+    const url = `https://jsonplaceholder.typicode.com/${resource}/${
+      rowNum ?? ""
+    }`;
 
-  const xhr = new XMLHttpRequest();
-  xhr.open(`GET`, url, true);
+    const xhr = new XMLHttpRequest();
+    xhr.open(`GET`, url, true);
 
-  xhr.addEventListener(`load`, () => {
-    if (xhr.status === 200) {
-      const result = JSON.parse(xhr.responseText);
-      console.log(result);
-      return result;
-    }
+    let result = [];
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        result = JSON.parse(xhr.responseText);
+        result.push(JSON.parse(xhr.responseText));
+      }
+    };
+    xhr.send();
   });
-  xhr.send();
 };
