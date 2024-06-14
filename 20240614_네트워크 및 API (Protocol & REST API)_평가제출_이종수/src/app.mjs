a51ed7500bba7e5db532dbc85b54e061fede0777
eonduck2 <whdtnxd@gmail.com>
2024-06-14 12:01:15 +0900

    비동기적 통신, 모듈화 완료

--- 20240614_네트워크 및 API (Protocol & REST API)_평가제출_이종수/src/app.mjs
+++ 20240614_네트워크 및 API (Protocol & REST API)_평가제출_이종수/src/app.mjs
@@ -1,11 +1,6 @@
 import resources from "./modules/AJAX/resources.js";
 import xhr from "./modules/AJAX/ajax.js";
 
-console.log(
-  xhr(resources.albums).then((res) => {
-    if (res.ok) {
-      console.log(res);
-      return res.json();
-    }
-  })
-);
+window.onload = async () => {
+  await xhr(resources.albums);
+};
--- 20240614_네트워크 및 API (Protocol & REST API)_평가제출_이종수/src/modules/AJAX/ajax.js
+++ 20240614_네트워크 및 API (Protocol & REST API)_평가제출_이종수/src/modules/AJAX/ajax.js
@@ -4,7 +4,7 @@
  * @param {Number} rowNum id 값으로 검색, 전달된 값 없을 시, 전체 리스트 조회
  */
 export default (resource, rowNum) => {
-  return new Promise((reslove, reject) => {
+  return new Promise((resolve, reject) => {
     const url = `https://jsonplaceholder.typicode.com/${resource}/${
       rowNum ?? ""
     }`;
@@ -12,11 +12,10 @@ export default (resource, rowNum) => {
     const xhr = new XMLHttpRequest();
     xhr.open(`GET`, url, true);
 
-    let result = [];
     xhr.onload = () => {
       if (xhr.status === 200) {
-        result = JSON.parse(xhr.responseText);
-        result.push(JSON.parse(xhr.responseText));
+        const result = JSON.parse(xhr.responseText);
+        resolve(result);
       }
     };
     xhr.send();
